38f6160a983ef7103459279f6512a3dac0113cd5
Ethan Mai <57975262+iAmEthanMai@users.noreply.github.com>
2022-02-14 15:23:52 -0500

    Update Footer.js

--- aurorae-cerebrum/src/Footer.js
+++ aurorae-cerebrum/src/Footer.js
@@ -8,7 +8,8 @@ function Footer(props) {
     return (
         <nav className="text-dark font-anaheim static bottom-0 h-16  bg-white pb-4">  
             <p className="justify-self-start p-4 pl-8 text-2xl">aurorae cerebrum</p>
-            <div className="pl-8 pr-8 p-4 grid sm:grid-cols-3 lg:grid-cols-4 gap-8">
+            <div className="pl-8 pr-8 p-4 flex grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                
                 <div>
                     <p className="text-lg sm:text-xl pb-2">à propos</p>
                     <div className="border-t border-dark border-opacity-10">
@@ -21,9 +22,9 @@ function Footer(props) {
                     <p className="text-lg sm:text-xl pb-2">produit</p>
                     <div className="border-t border-dark border-opacity-10">
                         <p className="text-base sm:text-lg text-opacity-50 text-dark pt-2"><a href=''>services</a></p>
-                        <p className="text-base sm:text-xl text-opacity-50 text-dark"><a href=''>plateforme</a></p>
+                        <p className="text-base sm:text-lg text-opacity-50 text-dark"><a href=''>plateforme</a></p>
                     </div>
-                </div>
+                </div>                
                 <div>
                     <p className="text-lg sm:text-xl pb-2">solutions</p>
                     <div className="text-base sm:text-lg border-t border-dark border-opacity-10">
@@ -32,15 +33,16 @@ function Footer(props) {
                     </div>
                 </div>    
             </div>
+
             <ul className="pl-8 pr-8 p-4 flex flex-col-reverse sm:flex-row items-left sm:items-center justify-between">
                 <li><p className="text-xs sm:text-base text-opacity-50 text-dark">©2022 aurorae cerebrum. all rights reserved</p></li>
                 <li className="pb-4 sm:pb-1">
-                    <ul className="flex flex space-x-4">
+                    <ul className="flex space-x-4">
                         <li><a href=''><Twitter/></a></li>
                         <li><a href=''><Discord/></a></li>
                         <li><a href='https://github.com/Aurorae-Cerebrum'><Github/></a></li>
                         <li><a href=''><Medium/></a></li>
-                        <li><a href=''><Linkedin/></a></li>
+                        <li><a href='https://www.linkedin.com/company/auror%C3%A6-cerebrum/about/'><Linkedin/></a></li>
                     </ul>
                 </li>
             </ul>
@@ -49,3 +51,7 @@ function Footer(props) {
 }
 
 export default Footer;
+
+
+
+//lg:grid-cols-4
